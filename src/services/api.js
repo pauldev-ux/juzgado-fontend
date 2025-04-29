8e92354ab692a1fdd7522a1b7069fb321a7ff42d
mastercloud0608 <jabrunostroebel@gmail.com>
2025-04-29 05:42:04 -0400

    holaa1

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,17 @@
 // src/services/api.js
-import axios from 'axios';
+/*import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:3001/api', // Ajusta esta URL a la de tu backend
 });
 
 export default api;
+
+*/
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL  // en lugar de la URL fija
+});
+
+export default API;
